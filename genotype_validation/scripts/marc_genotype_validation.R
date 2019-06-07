6875890382a699388a1ed90cebb3322c2f84343e
James Ta <jkta@ucdavis.edu>
2019-06-07 10:23:34 -0700

    stuff

--- genotype_validation/scripts/marc_genotype_validation.R
+++ genotype_validation/scripts/marc_genotype_validation.R
@@ -1,10 +1,11 @@
 #this script will verify the genotypes from published marker data to Marc's marker data
 #populations are 21RV, 20RV, 8RV, 29RV, 28RV, 27RV, 13RV
+library(data.table)
 
 rm(list = ls())
 
 #first reading in the genotype probability file
-setwd("/Users/James/Documents/GitHub/sar_qtl/6_push_button/")
+setwd("/Users/jkhta/Documents/GitHub/sar_qtl/6_push_button/")
 geno_array <- readRDS("nam_rqtl_geno_prob_array_final_0.99_no_kinship.rds")
 marker_anno <- fread("nam_marker_info_final.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
 marker_anno$snp <- paste("m", marker_anno$snp, sep = "_")
@@ -18,8 +19,6 @@ fri_flc_qtl <- c("m_4_407208", "m_5_3799350")
 names(geno_array) <- paste("m", names(geno_array), sep = "_")
 geno_array_FRI_FLC <- geno_array[fri_flc_qtl]
 
-library(data.table)
-
 #grabbing genotypes for FRI and FLC individually to compare against the past genotype data
 geno_array_FRI_FLC_col <- do.call(cbind, lapply(geno_array_FRI_FLC, function(x) ifelse(x[, 1] >= 0.5, "A", "B")))
 geno_array_FRI <- data.frame(geno = rownames(geno_array_FRI_FLC_col), genotypes = geno_array_FRI_FLC_col[, 1])
@@ -30,7 +29,7 @@ marker_anno_FRI_FLC <- subset(marker_anno, snp %in% fri_flc_qtl)
 marker_anno_FRI_FLC$Start_Mb <- as.numeric(sapply(strsplit(marker_anno_FRI_FLC$snp, split = "_"), function(x) x[3]))
 
 #reading in the past genotypes data
-setwd("/Users/James/Documents/GitHub/sar_qtl/genotype_validation/genotype/")
+setwd("/Users/jkhta/Documents/GitHub/sar_qtl/genotype_validation/genotype/")
 past_genotypes <- lapply(list.files(pattern = "RV_genotypes.csv"), function(x) fread(x, sep = ",", header = TRUE, stringsAsFactors = FALSE))
 genotype_names <- sapply(strsplit(list.files(pattern = "RV_genotypes.csv"), split = "_"), function(x) x[1])
 
@@ -57,24 +56,40 @@ past_genotypes_chr_4_Mb_min <- lapply(past_genotypes_chr_4, function(x) subset(x
 past_genotypes_FRI <- lapply(past_genotypes_chr_4, function(x) t(subset(x, cM_diff == min(cM_diff))))
 past_genotypes_FRI <- lapply(past_genotypes_chr_4, function(x) t(subset(x, Mb_diff == min(Mb_diff))))
 
+past_genotypes_FRI_dist <- unlist(lapply(past_genotypes_chr_4, function(x) subset(x, cM_diff == min(cM_diff))$cM_diff))
+past_genotypes_FRI_dist <- unlist(lapply(past_genotypes_chr_4, function(x) subset(x, Mb_diff == min(Mb_diff))$Mb_diff))
+
 #genotype plots
 FRI_correlation <- c()
 
 for (i in 1:length(past_genotypes_FRI)) {
     past_genotypes_FRI[[i]] <- data.frame(geno = rownames(past_genotypes_FRI[[i]]), genotypes = past_genotypes_FRI[[i]][, 1])
     FRI_merged <- merge(geno_array_FRI, past_genotypes_FRI[[i]], by = "geno")
+    FRI_merged[FRI_merged == "NA"] <- NA
+    FRI_merged[, 2] <- factor(FRI_merged[, 2], levels = c("A", "B"))
+    FRI_merged[, 3] <- factor(FRI_merged[, 3], levels = c("A", "B"))
     FRI_cor <- with(FRI_merged, cor(as.numeric(genotypes.x), as.numeric(genotypes.y), use = "pairwise.complete.obs"))
     single_pop_FRI_cor <- data.frame(pop = genotype_names[i], cor = FRI_cor, num_geno = nrow(FRI_merged))
     FRI_correlation <- rbindlist(list(FRI_correlation, single_pop_FRI_cor))
 }
 
+#reading out the correlation information between Marc's genotypes and the publicly available genotypes
+FRI_correlation$dist <- past_genotypes_FRI_dist
 FRI_correlation
 
+#now working on the chromosome 5 genotypes
 past_genotypes_chr_5 <- lapply(past_genotypes, function(x) subset(x, Chr == 5))
 
 for (i in 1:length(past_genotypes_chr_5)) {
     past_genotypes_chr_5[[i]]$cM_diff <- abs(past_genotypes_chr_5[[i]]$CR_cM - marker_anno_FRI_FLC$cM[2])
     past_genotypes_chr_5[[i]]$Mb_diff <- abs(past_genotypes_chr_5[[i]]$Start_Mb * 1000000 - marker_anno_FRI_FLC$Start_Mb[2])
+}
+
+#for farthest marker
+for (i in 1:length(past_genotypes_chr_5)) {
+    past_genotypes_chr_5[[i]]$cM_diff <- abs(past_genotypes_chr_5[[i]]$CR_cM - marker_anno_FRI_FLC$cM[2])
+    past_genotypes_chr_5[[i]]$Mb_diff <- abs(abs(((past_genotypes_chr_5[[i]]$Start_Mb * 1000000) - max(past_genotypes_chr_5[[i]]$Start_Mb * 1000000)))
+ - marker_anno_FRI_FLC$Start_Mb[2])
 }
 
 past_genotypes_chr_5_cM_min <- lapply(past_genotypes_chr_5, function(x) subset(x, cM_diff == min(cM_diff))$cM_diff)
@@ -83,16 +98,23 @@ past_genotypes_chr_5_Mb_min <- lapply(past_genotypes_chr_5, function(x) subset(x
 past_genotypes_FLC <- lapply(past_genotypes_chr_5, function(x) t(subset(x, cM_diff == min(cM_diff))))
 past_genotypes_FLC <- lapply(past_genotypes_chr_5, function(x) t(subset(x, Mb_diff == min(Mb_diff))))
 
+past_genotypes_FLC_dist <- unlist(lapply(past_genotypes_chr_5, function(x) subset(x, cM_diff == min(cM_diff))$cM_diff))
+past_genotypes_FLC_dist <- unlist(lapply(past_genotypes_chr_5, function(x) subset(x, Mb_diff == min(Mb_diff))$Mb_diff))
+
 #genotype plots
 FLC_correlation <- c()
 
 for (i in 1:length(past_genotypes_FLC)) {
     past_genotypes_FLC[[i]] <- data.frame(geno = rownames(past_genotypes_FLC[[i]]), genotypes = past_genotypes_FLC[[i]][, 1])
-    FLC_merged <- merge(geno_array_FRI, past_genotypes_FLC[[i]], by = "geno")
+    FLC_merged <- merge(geno_array_FLC, past_genotypes_FLC[[i]], by = "geno")
+    FLC_merged[FLC_merged == "NA"] <- NA
+    FLC_merged[, 2] <- factor(FLC_merged[, 2], levels = c("A", "B"))
+    FLC_merged[, 3] <- factor(FLC_merged[, 3], levels = c("A", "B"))
     FLC_cor <- with(FLC_merged, cor(as.numeric(genotypes.x), as.numeric(genotypes.y), use = "pairwise.complete.obs"))
     single_pop_FLC_cor <- data.frame(pop = genotype_names[i], cor = FLC_cor, num_geno = nrow(FLC_merged))
     FLC_correlation <- rbindlist(list(FLC_correlation, single_pop_FLC_cor))
 }
 
+FLC_correlation$dist <- past_genotypes_FLC_dist
 FLC_correlation
 
