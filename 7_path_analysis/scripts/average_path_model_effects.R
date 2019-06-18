cc34ebf2cad5e36c55ba023dea62f35ed7098e70
James Ta <jkta@ucdavis.edu>
2019-06-18 08:53:48 -0700

    path average figures

--- 7_path_analysis/scripts/average_path_model_effects.R
+++ 7_path_analysis/scripts/average_path_model_effects.R
@@ -9,13 +9,13 @@ pop_qtl_eff_list_sun <- as.data.frame(rbindlist(lapply(list.files(pattern = "qtl
 pop_qtl_eff_list_sun[is.na(pop_qtl_eff_list_sun)] <- 0
 pop_qtl_eff_list_sun_avg <- apply(pop_qtl_eff_list_sun[,grepl("~", colnames(pop_qtl_eff_list_sun))], 2, function(x) mean(x))
 
-pop_qtl_eff_list_shade <- as.data.frame(rbindlist(lapply(list.files(pattern = "qtl_eff_alt_shade.csv"), function(x) fread(x, sep = ",", header = TRUE, stringsAsFactors = FALSE))))
-pop_qtl_eff_list_shade[is.na(pop_qtl_eff_list_shade)] <- 0
-pop_qtl_eff_list_shade_avg <- apply(pop_qtl_eff_list_shade[,grepl("~", colnames(pop_qtl_eff_list_shade))], 2, function(x) mean(x))
-
 pop_trait_eff_list_sun <- as.data.frame(rbindlist(lapply(list.files(pattern = "trait_eff_alt_sun.csv"), function(x) fread(x, sep = ",", header = TRUE, stringsAsFactors = FALSE))))
 pop_trait_eff_list_sun[is.na(pop_trait_eff_list_sun)] <- 0
 pop_trait_eff_list_sun_avg <- apply(pop_trait_eff_list_sun[,grepl("~", colnames(pop_trait_eff_list_sun))], 2, function(x) mean(x))
+
+pop_qtl_eff_list_shade <- as.data.frame(rbindlist(lapply(list.files(pattern = "qtl_eff_alt_shade.csv"), function(x) fread(x, sep = ",", header = TRUE, stringsAsFactors = FALSE))))
+pop_qtl_eff_list_shade[is.na(pop_qtl_eff_list_shade)] <- 0
+pop_qtl_eff_list_shade_avg <- apply(pop_qtl_eff_list_shade[,grepl("~", colnames(pop_qtl_eff_list_shade))], 2, function(x) mean(x))
 
 pop_trait_eff_list_shade <- as.data.frame(rbindlist(lapply(list.files(pattern = "trait_eff_alt_shade.csv"), function(x) fread(x, sep = ",", header = TRUE, stringsAsFactors = FALSE))))
 pop_trait_eff_list_shade[is.na(pop_trait_eff_list_shade)] <- 0
