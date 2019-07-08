78ff1419c14f1065e5cf52a687a03803ca7eeb22
James Ta <jkta@ucdavis.edu>
2019-07-08 11:54:26 -0700

    GEMMA, TASSEL, and RTM-GWAS runs of GxE bolting time

--- figures/GWAS_comparisons/tassel_rtm_gwas_data_preparer.R
+++ figures/GWAS_comparisons/tassel_rtm_gwas_data_preparer.R
@@ -4,7 +4,7 @@ library(data.table)
 rm(list = ls())
 
 #reading in blups
-setwd("/Users/James/Documents/GitHub/sar_qtl/figures/path_analysis_GridLMM/")
+setwd("/Users/jkhta/Documents/GitHub/sar_qtl/figures/path_analysis_GridLMM/")
 
 nam_blups <- fread("nam_blups_combined_univariate.csv",
                    sep = ",",
@@ -16,15 +16,22 @@ nam_family <- paste(sapply(strsplit(nam_blups$geno, split = "RV"), function(x) x
 nam_blups$geno <- paste(nam_family, nam_blups$geno, sep = "_")
 
 #grabbing only bolting time
-nam_blups_subset <- subset(nam_blups, select = c(geno, bd_geno))
+nam_bd_geno_subset <- subset(nam_blups, select = c(geno, bd_geno))
+nam_bd_gxe_subset <- subset(nam_blups, select = c(geno, bd_gxe))
 
 #saving files
-setwd("/Users/James/Documents/GitHub/sar_qtl/figures/GWAS_comparisons/")
+setwd("/Users/jkhta/Documents/GitHub/sar_qtl/figures/GWAS_comparisons/")
 
 #saving file for tassel
-colnames(nam_blups_subset) <- c("<Trait>", "bd")
-fwrite(nam_blups_subset, "bd_geno_tassel.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+colnames(nam_bd_geno_subset) <- c("<Trait>", "bd")
+fwrite(nam_bd_geno_subset, "bd_geno_tassel.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+
+colnames(nam_bd_gxe_subset) <- c("<Trait>", "bd")
+fwrite(nam_bd_gxe_subset, "bd_gxe_tassel.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 
 #saving file for rtm-gwas
-colnames(nam_blups_subset) <- c("Indiv", "bd")
-fwrite(nam_blups_subset, "bd_geno_rtm_gwas.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+colnames(nam_bd_geno_subset) <- c("Indiv", "bd")
+fwrite(nam_bd_geno_subset, "bd_geno_rtm_gwas.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+
+colnames(nam_bd_gxe_subset) <- c("Indiv", "bd")
+fwrite(nam_bd_gxe_subset, "bd_gxe_rtm_gwas.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
