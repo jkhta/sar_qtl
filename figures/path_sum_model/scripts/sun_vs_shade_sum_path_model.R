8f6b5b48b7e37c73affc3d2c9b6e16ac0e1ccb25
James Ta <jkta@ucdavis.edu>
2019-08-22 20:31:08 -0700

    Changed trait names in path analysis figure

--- figures/path_sum_model/scripts/sun_vs_shade_sum_path_model.R
+++ figures/path_sum_model/scripts/sun_vs_shade_sum_path_model.R
@@ -6,8 +6,8 @@ rm(list = ls())
 
 setwd("/Users/James/Documents/GitHub/sar_qtl/figures/path_sum_model/img/")
 
-sum_path_sun <- readPNG("sig_paths_sum_sun_model.png")
-sum_path_shade <- readPNG("sig_paths_sum_shade_model.png")
+sum_path_sun <- readPNG("sig_paths_sum_sun_model_alt.png")
+sum_path_shade <- readPNG("sig_paths_sum_shade_model_alt.png")
 
 im_A <- ggplot() + 
     background_image(sum_path_sun) +
