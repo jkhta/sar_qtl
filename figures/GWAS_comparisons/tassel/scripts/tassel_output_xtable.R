5acdd5972316db4cd58bbf87f5e05606ca9490d3
James Ta <jkta@ucdavis.edu>
2020-03-25 10:32:25 -0700

    tassel family output

--- figures/GWAS_comparisons/tassel/scripts/tassel_output_xtable.R
+++ figures/GWAS_comparisons/tassel/scripts/tassel_output_xtable.R
@@ -14,10 +14,13 @@ tassel_bd_geno_subset <- tassel_bd_geno_subset[-c(1, nrow(tassel_bd_geno_subset)
 print(xtable(tassel_bd_geno_subset), include.rownames = FALSE)
 
 #xtable for bd_gxe
-tassel_bd_gxe <- fread("tassel_bd_gxe_output.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+#tassel_bd_gxe <- fread("tassel_bd_gxe_output.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+tassel_bd_gxe <- fread("tassel_bd_gxe_output_revised_with_family.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
 tassel_bd_gxe_subset <- subset(tassel_bd_gxe, select = c(1, 2, 3, 4, 9))
 
 tassel_bd_gxe_subset <- tassel_bd_gxe_subset[-c(1, nrow(tassel_bd_gxe_subset)), ]
 
+setwd("../figure/")
+fwrite(tassel_bd_gxe_subset, "table_s5_tassel_bd_gxe_results.csv", sep = ",", row.names = FALSE)
 print(xtable(tassel_bd_gxe_subset), include.rownames = FALSE)
